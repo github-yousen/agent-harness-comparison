6cde6ccc3c07b73854e88766d20ed9adf6c0095f
yousen <2540462586@qq.com>
2026-06-05 02:20:37 +0800

    remove: 删除 code/ 文件夹

--- code/opencode/core/src/session/error.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from "effect"
-import { SessionMessage } from "./message"
-import { SessionSchema } from "./schema"
-
-export class MessageDecodeError extends Schema.TaggedErrorClass<MessageDecodeError>()("Session.MessageDecodeError", {
-  sessionID: SessionSchema.ID,
-  messageID: SessionMessage.ID,
-}) {}
